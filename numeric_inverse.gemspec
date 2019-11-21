b839816320bfd5df0c1cd37d3034777739d6b8b9
Masahiro Nomoto <hmmnrst@users.noreply.github.com>
2019-11-22 03:07:21 +0900

    Enable rspec `aggregate_failures` metadata

--- numeric_inverse.gemspec
+++ numeric_inverse.gemspec
@@ -25,5 +25,5 @@ Gem::Specification.new do |spec|
 
 	spec.add_development_dependency "bundler"
 	spec.add_development_dependency "rake"
-	spec.add_development_dependency "rspec", "~> 3.0"
+	spec.add_development_dependency "rspec", "~> 3.3"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true unless meta.key?(:aggregate_failures)
+  end
 end
